737a6f32eda889a714f3e0184f1ab9e956ebab1f
wey-gu <1651790+wey-gu@users.noreply.github.com>
2021-10-29 21:56:55 +0800

    minor: debugging function

--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ from siwi import app
 
 application = app = app.app
 
-def siwi_api():
-   application.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+def siwi_api(request):
+    print(str(request))
+    application.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
