9d66b3f89b7843bbee4d1d219bde38acf99acc8c
wey-gu <1651790+wey-gu@users.noreply.github.com>
2021-10-29 22:11:47 +0800

    openfunction adaptation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
-from siwi import app
+from nebula2.gclient.net import ConnectionPool
+from nebula2.Config import Config
+from siwi.app import parse_nebula_graphd_endpoint
+from siwi.bot import bot
 
-application = app = app.app
+
+ng_config = Config()
+ng_config.max_connection_pool_size = int(
+    os.environ.get('NG_MAX_CONN_POOL_SIZE', 10))
+ng_endpoints = parse_nebula_graphd_endpoint()
+connection_pool = ConnectionPool()
+connection_pool.init(ng_endpoints, ng_config)
+siwi_bot = bot.SiwiBot(connection_pool)
+
 
 def siwi_api(request):
-    print(str(request))
-    application.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    request_data = request.get_json()
+    question = request_data.get("question", "")
+    if question:
+        answer = siwi_bot.query(request_data.get("question", ""))
+    else:
+        answer = "Sorry, what did you say?"
+    return jsonify({"answer": answer})
